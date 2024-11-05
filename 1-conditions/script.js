dfeddf6cecbad6e1a300df3a645436481809a801
Farid Shahmurov <154803158+feridshahmur@users.noreply.github.com>
2024-11-05 14:29:05 +0400

    1-conditions

---
+++ 1-conditions/script.js
@@ -0,0 +1,55 @@
+// task1(bank meselesi)
+// let emekHaqqi = +prompt("emek haqqinizi daxil edin");
+// let istediyinizMeblegh = +prompt("istediyiniz meblegi daxil edin");
+// let istediyinizMuddet = +prompt("ayi daxil edin");
+// if (istediyinizMuddet > 36) {
+//     alert("Size bu shertlerle kredit dushmur");
+// } else if (istediyinizMeblegh > emekHaqqi * 10) {
+//     alert("Size bu shertlerle kredit dushmur");
+// } else {
+//     let faiz;
+//     if (istediyinizMuddet <= 12) {
+//         faiz = 0.13;
+//     } else if (istediyinizMuddet <= 24) {
+//         faiz = 0.15;
+//     } else {
+//         faiz = 0.17;
+//     }
+//     let ayliqOdenis;
+//     let kreditMeblegi;
+//     if (kreditMeblegi>emekHaqqi*10) {
+//         alert("Size bu shertlerle kredit dushmur");
+//     }else if(ayliqOdenis>(emekHaqqi*40/100)){
+//         alert("Size bu shertlerle kredit dushmur");
+//     }else{
+//         ayliqOdenis=((istediyinizMeblegh*faiz)/istediyinizMuddet);
+//     }
+//  let yekun=(`${istediyinizMeblegh}  mebleg size ${faiz} le ${istediyinizMuddet} aya muddetine ayliq ${ayliqOdenis} manat vwerili`)
+//  alert(yekun)
+// }
+// task2
+// let a = +prompt("a ededini daxil edin");
+// let b = +prompt("b ededini daxil edin");
+// let c = +prompt("c ededini daxil edin");
+// if (a+b>a+c && a+b>b+c) {
+//     alert(`Big sum result ${a} and ${b}`)
+// } else if (a+c>a+b && a+c>b+c) {
+//     alert(`Big sum result ${a} and ${c}`)
+// }else{
+//     alert(`Big sum result ${b} and ${c}`)
+// }
+// task3
+// 3 ədəd verilib. Bu ədədlərin birləşməsindən yaranan ən böyük və ən kiçik 3 rəqəmli ədədi tapan proqram tərtib edin. Məsələn: a=4, b=5, c=6 daxil etdikdə cavab bu şəkildə olmalıdır:
+// Big number: 654 
+// Little number: 456 
+// 
+// task4
+let katet1 = +prompt("1. kateti daxil edin");
+let katet2 = +prompt("2. kateti daxil edin");
+let katet3 = +prompt("3. kateti daxil edin");
+if (katet3**=(katet1**2+katet2**2)) {
+    
+} else {
+    
+}
+
