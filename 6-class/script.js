2a10575c7ae4241bac12e8cae6279b4bf895657c
feridshahmur <154803158+feridshahmur@users.noreply.github.com>
2024-11-07 00:21:37 +0400

    java script class

---
+++ 6-class/script.js
@@ -0,0 +1,46 @@
+// 1)Rectangle adında bir class yaradın.
+// Rectangle class-ına calculateArea() adlı bir metod daxil edin. Bu metod width və height dəyərlərini istifadə edərək düzbucaqlının sahəsini hesablayacaq və geri qaytaracaq.
+
+// Rectangle class-ına calculatePerimeter() adlı başqa bir metod daxil edin.
+// Bu metod width və height dəyərlərini istifadə edərək düzbucaqlının perimetrini hesablayacaq və geri qaytaracaq.
+
+// Reactangle sinifinin instanceni yaradın və onun sahəsini və perimetrini hesablayın.
+
+// Nümunə:
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+   
+//     calculateArea() {
+//        ...
+//     }
+
+//     calculatePerimeter() {
+//        ...
+//     }
+// }
+
+// //instance of Reactangle class
+// const myRectangle = new Rectangle(5, 10);
+
+// console.log("Area of the rectangle:", myRectangle.calculateArea()); //  50
+// console.log("Perimeter of the rectangle:", myRectangle.calculatePerimeter()); // 30
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+//     calculateArea() {
+//         return this.width * this.height;
+//     }
+//     calculatePerimeter() {
+//         return 2 * (this.width + this.height);
+//     }
+// }
+// const myRectangle = new Rectangle(5, 10);
+// console.log(myRectangle.calculateArea()); 
+// console.log(myRectangle.calculatePerimeter()); 
+
